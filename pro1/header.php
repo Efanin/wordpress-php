dc938da47494eb635ded34d015c169eadc16328a
Igot <164139862+Efanin@users.noreply.github.com>
2026-01-28 16:55:00 +0300

    Add files via upload

---
+++ pro1/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Блог</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4">Simple header</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="index.php" class="nav-link active" aria-current="page">Блог</a></li>
+        <li class="nav-item"><a href="price.php" class="nav-link">Цены</a></li>
+        <li class="nav-item"><a href="order.php" class="nav-link">Заказ</a></li>
+        <li class="nav-item"><a href="game.php" class="nav-link">Игры</a></li>
+        <li class="nav-item"><a href="cats.php" class="nav-link">Котики</a></li>
+      </ul>
+    </header>
+  </div>
